229513e7d3d64044c1325ca1e79424012b293df3
ユーマヨ <scha-taz0@outlook.jp>
2017-10-09 19:09:32 +0900

    include/Network/Packet/PacketId.hを解析するexeを作成しました。
    中にあるenum値からクラスのヘッダーとソースをそれぞれ規定のディレクトリに、
    作成するようにしました。
    すでにファイルがある場合は上書きされません。

---
+++ mogupro/game/pmake/TEMPLATE.cpp
@@ -0,0 +1,22 @@
+#include <Network/Packet/_PACKET_TYPE_/_TEMPLATE_CLASS_.h>
+namespace Network
+{
+namespace Packet
+{
+namespace _PACKET_TYPE_
+{
+_TEMPLATE_CLASS_::_TEMPLATE_CLASS_( )
+{
+
+}
+void _TEMPLATE_CLASS_::packetImport( ubyte2 size, char const* const data )
+{
+
+}
+ubyte2 _TEMPLATE_CLASS_::packetExport( char* const data )
+{
+    return 0;
+}
+}
+}
+}
