ad9cdd96bc4a8b8df3e61ae8612f4c1aac55f458
zun-ferrum <dry.tm.fe@icloud.com>
2017-10-12 16:22:59 +0900

    Update : cReqPlayerにImport、Export追加

--- mogupro/game/src/Network/Packet/Request/cReqPlayer.cpp
+++ mogupro/game/src/Network/Packet/Request/cReqPlayer.cpp
@@ -1,4 +1,5 @@
 #include <Network/Packet/Request/cReqPlayer.h>
+#include <Network/PackFunction.hpp>
 namespace Network
 {
 namespace Packet
@@ -7,15 +8,36 @@ namespace Request
 {
 cReqPlayer::cReqPlayer( )
 {
-
+	xPos = yPos = zPos = -10.0f;
+	thetaX = 1.5f;
+	thetaY = 2.4f;
 }
 void cReqPlayer::packetImport( cNetworkHandle networkHandle, ubyte2 transferredBytes, char const* const data )
 {
+	bool end = true;
+	int offset = 0;
+	end = importFloat(xPos,data,offset,20);
+	end = importFloat(yPos, data, offset, 20);
+	end = importFloat(zPos, data, offset, 20);
+	end = importFloat(thetaX, data, offset, 20);
+	end = importFloat(thetaY, data, offset, 20);
 
+	//if (end != true)
 }
 ubyte2 cReqPlayer::packetExport( char* const data )
 {
-    return 0;
+	bool end = true;
+	int offset = 0;
+	end = exportFloat(xPos,data,offset,20);
+	end = exportFloat(yPos, data, offset, 20);
+	end = exportFloat(zPos, data, offset, 20);
+	end = exportFloat(thetaX, data, offset, 20);
+	end = exportFloat(thetaY, data, offset, 20);
+
+	//if(end != true)
+		
+
+    return offset;
 }
 }
 }
