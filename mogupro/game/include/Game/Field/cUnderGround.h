21e001f30341a3fd1fb4b096c1ccd509330043b9
yudai-kote <yu1229kotetu@i.softbank.jp>
2017-11-12 12:52:07 +0900

    ブロック破壊関数を修正しました

--- mogupro/game/include/Game/Field/cUnderGround.h
+++ mogupro/game/include/Game/Field/cUnderGround.h
@@ -30,7 +30,7 @@ public: // Player,Strategy Ç≈égÇ§ä÷êî
     bool blockBreakNetwork( const ci::vec3& position, const float & radius, const cBreakBlockType& type );
     ci::vec3 getBlockTopPosition( const ci::vec3& target_position );
     ci::vec3 getBlockHighestPosition( const ci::vec3& target_position );
-    bool isBreakBlock( const vec3& position, const float& raiuds );
+    bool isBreakBlock( const ci::vec3& position, const float& radius );
 
 public: // Field ì‡Ç≈égÇ§ä÷êî
 
@@ -40,18 +40,17 @@ public: // Field ì‡Ç≈égÇ§ä÷êî
 private: // Method
 
     bool chunkMeshReLoaded();
-    bool calcChunks();
+    bool createChunks();
 
     ci::ivec3 getChunkCellFromPosition( const ci::vec3& position );
     ci::ivec3 getBlockCellFromPosition( const ci::vec3& position );
 
 private: // Member
 
-    cChunkHolder mChunkHolder;
+    cChunkHolder *mChunkHolder;
     std::vector<std::thread> mChunkLoadThreads;
     std::mutex mMainMutex;
     std::atomic<bool> mIsRunning{ true };
-    std::function<void()> mCallBack = nullptr;
 
 };
 }
