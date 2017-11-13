6703c0ee7c77dc239d26424b396153fb5ecdbe87
ユーマヨ <scha-taz0@outlook.jp>
2017-11-14 01:00:57 +0900

    ラインライトのテスト、プレイヤーの軌跡が光となって残るようにした。

---
+++ mogupro/game/include/Game/Light/cLineLightParam.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <cinder/Vector.h>
+#include <Utility/cUserPointer.hpp>
+namespace Game
+{
+namespace Light
+{
+class cLineLightParam
+{
+public:
+	cLineLightParam( cinder::vec3 beginPosition, cinder::vec3 endPosition, cinder::vec3 color, float radius )
+		: beginPosition( beginPosition )
+		, endPosition( endPosition )
+		, color( color )
+		, radius( radius )
+	{
+	}
+public:
+	void reAttachLine( Utility::softptr<cLineLightParam> handle, cinder::vec3 beginPosition, cinder::vec3 endPosition );
+	void reAttachRadius( Utility::softptr<cLineLightParam> handle, float radius );
+	void reAttachLineWithRadius( Utility::softptr<cLineLightParam> handle, cinder::vec3 beginPosition, cinder::vec3 endPosition, float radius );
+	inline cinder::vec3 getBeginPosition( )
+	{
+		return beginPosition;
+	}
+	inline cinder::vec3 getEndPosition( )
+	{
+		return endPosition;
+	}
+	inline float getRadius( )
+	{
+		return radius;
+	}
+private:
+	cinder::vec3 beginPosition;
+	cinder::vec3 endPosition;
+	float radius;
+public:
+	cinder::vec3 color;
+};
+}
+}
--- mogupro/game/include/Game/cLightManager.h
+++ mogupro/game/include/Game/cLightManager.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include <Utility/cUserPointer.hpp>
 #include <Game/Light/cPointLightParam.h>
+#include <Game/Light/cLineLightParam.h>
 #include <boost/optional.hpp>
 namespace Game
 {
@@ -17,6 +18,7 @@ public:
 	void setup( );
 	void update( );
 
+public: // ポイントライト
 	std::set<Utility::hardptr<Light::cPointLightParam>>const& getPointLights( ) const;
 	boost::optional<std::set<Utility::softptr<Light::cPointLightParam>>const&> getPointLights( int chunkId ) const;
 	Utility::softptr<Light::cPointLightParam> addPointLight( cinder::vec3 position, cinder::vec3 color, float radius );
@@ -26,7 +28,21 @@ public:
 private:
 	std::set<Utility::hardptr<Light::cPointLightParam>> mPointLights;
 	std::map<int, std::set<Utility::softptr<Light::cPointLightParam>>> mPointLightsMap;
+
+public: // ラインライト
+	std::set<Utility::hardptr<Light::cLineLightParam>>const& getLineLights( ) const;
+	boost::optional<std::set<Utility::softptr<Light::cLineLightParam>>const&> getLineLights( int chunkId ) const;
+	Utility::softptr<Light::cLineLightParam> addLineLight( cinder::vec3 beginPosition, cinder::vec3 endPosition, cinder::vec3 color, float radius );
+	void removeLineLight( Utility::softptr<Light::cLineLightParam> handle );
+	void attachChunk( Utility::softptr<Light::cLineLightParam> handle );
+	void detachChunk( Utility::softptr<Light::cLineLightParam> handle );
 private:
+	std::set<Utility::hardptr<Light::cLineLightParam>> mLineLights;
+	std::map<int, std::set<Utility::softptr<Light::cLineLightParam>>> mLineLightsMap;
+
+private: // プレイヤー更新用。
 	std::vector<Utility::softptr<Light::cPointLightParam>> mPointLightHandles;
+	std::vector<cinder::vec3> mPlayerPositionBuffer;
+	std::vector<Utility::softptr<Light::cLineLightParam>> mLineLightHandles;
 };
 }
