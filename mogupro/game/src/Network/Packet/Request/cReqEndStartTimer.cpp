029cc15dbf507cca39dd09f0d213ac08b5a82622
zun-ferrum <dry.tm.fe@icloud.com>
2017-11-16 20:20:08 +0900

    Fix : 修正

---
+++ mogupro/game/src/Network/Packet/Request/cReqEndStartTimer.cpp
@@ -0,0 +1,26 @@
+#include <Network/Packet/Request/cReqEndStartTimer.h>
+#include <Network/PackFunction.hpp>
+namespace Network
+{
+namespace Packet
+{
+namespace Request
+{
+cReqEndStartTimer::cReqEndStartTimer( )
+{
+
+}
+void cReqEndStartTimer::packetImport( cNetworkHandle networkHandle, ubyte2 transferredBytes, char const* const data )
+{
+	mNetworkHandle = networkHandle; 
+}
+ubyte2 cReqEndStartTimer::packetExport( char* const data )
+{
+    cExporter exp( data );
+    // 使い方: vec2 pos1, pos2; ubyte2 a1, a2;
+    // exp << pos1 << a1 << pos2 << a2;
+    return exp;
+}
+}
+}
+}
